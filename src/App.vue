6a2228544a973f9ab483f071fc71468d8d01c5ac
jlyonas <jlyon@acumensolutions.com>
2017-12-18 20:57:45 -0500

    add theme picker

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,35 @@
 <template>
-	<div id="app">
-		stuff
+	<div class="">
+		<v-app :dark="isDark">
+			<v-content>
+				<v-container>
+					<v-select
+						label="Theme"
+						v-model="selectedTheme"
+						:items="themes"
+						single-line
+						bottom ></v-select>
+				</v-container>
+			</v-content>
+		</v-app>
 	</div>
 </template>
 
 <script>
 export default {
-
+	data() {
+		return {
+			selectedTheme: "dark",
+			themes: [
+				{ text: 'Dark', value: 'dark' },
+				{ text: 'Light', value: 'light' }
+			]
+		}
+	},
+	computed: {
+		isDark() {
+			return this.selectedTheme === "dark";
+		}
+	}
 }
 </script>
